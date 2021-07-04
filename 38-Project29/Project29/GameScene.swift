9c7533e6e66163099efcc95ee79ba9dcc13a3846
Khumar Girdhar <77577795+khumargirdhar@users.noreply.github.com>
2021-07-04 19:46:19 +0530

    Add files via upload

---
+++ 38-Project29/Project29/GameScene.swift
@@ -0,0 +1,293 @@
+//
+//  GameScene.swift
+//  Project29
+//
+//  Created by Khumar Girdhar on 24/06/21.
+//
+
+import SpriteKit
+
+enum CollisionTypes: UInt32 {
+    case banana = 1
+    case building = 2
+    case player = 4
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    var buildings = [BuildingNode]()
+    weak var viewController: GameViewController?
+    
+    var player1: SKSpriteNode!
+    var player2: SKSpriteNode!
+    var banana: SKSpriteNode!
+    
+    var currentPlayer = 1
+    
+    //Challenge 1
+    var score1Label: SKLabelNode!
+    var score1 = 0
+    var score2Label: SKLabelNode!
+    var score2 = 0
+    
+    var windLabel: SKLabelNode!
+  
+    override func didMove(to view: SKView) {
+        backgroundColor = UIColor(hue: 0.669, saturation: 0.99, brightness: 0.67, alpha: 1)
+        
+        //Challenge 1
+        
+        let defaults = UserDefaults.standard
+        score1 = defaults.integer(forKey: "score1")
+        score2 = defaults.integer(forKey: "score2")
+        
+        score1Label = SKLabelNode(fontNamed: "Chalkduster")
+        score1Label.zPosition = 1
+        score1Label.position = CGPoint(x: 100, y: 680)
+        score1Label.fontSize = 32
+        score1Label.text = "Score: \(score1)"
+        addChild(score1Label)
+        
+        score2Label = SKLabelNode(fontNamed: "Chalkduster")
+        score2Label.zPosition = 1
+        score2Label.position = CGPoint(x: 920, y: 680)
+        score2Label.fontSize = 32
+        score2Label.text = "Score: \(score2)"
+        addChild(score2Label)
+        
+        createBuildings()
+        createPlayers()
+        
+        physicsWorld.contactDelegate = self
+        
+        let randomGravity = Int.random(in: -4...4)
+        windLabel = SKLabelNode(fontNamed: "Chalkduster")
+        windLabel.fontSize = 25
+        windLabel.text = "Wind: \(randomGravity)"
+        windLabel.zPosition = 1
+        windLabel.position = CGPoint(x: 512, y: 620)
+        physicsWorld.gravity = CGVector(dx: randomGravity, dy: -10)
+        addChild(windLabel)
+        
+    }
+    
+    func createBuildings() {
+        var currentX: CGFloat = -15
+        
+        while currentX < 1024 {
+            let size = CGSize(width: Int.random(in: 2...4) * 40, height: Int.random(in: 300...600))
+            
+            currentX += size.width + 2
+            
+            let building = BuildingNode(color: .red, size: size)
+            building.position = CGPoint(x: currentX - (size.width) / 2, y: size.height / 2)
+            building.setup()
+            addChild(building)
+            
+            buildings.append(building)
+        }
+    }
+    
+    func launch(angle: Int, velocity: Int) {
+        let speed = Double(velocity) / 10
+        let radians = deg2rad(degrees: angle)
+        
+        if banana != nil {
+            banana.removeFromParent()
+            banana = nil
+        }
+        
+        banana = SKSpriteNode(imageNamed: "banana")
+        banana.name = "banana"
+        banana.physicsBody = SKPhysicsBody(circleOfRadius: banana.size.width / 2)
+        banana.physicsBody?.categoryBitMask = CollisionTypes.banana.rawValue
+        banana.physicsBody?.collisionBitMask = CollisionTypes.building.rawValue | CollisionTypes.player.rawValue
+        banana.physicsBody?.contactTestBitMask = CollisionTypes.building.rawValue | CollisionTypes.player.rawValue
+        banana.physicsBody?.usesPreciseCollisionDetection = true
+        addChild(banana)
+        
+        if currentPlayer == 1 {
+            banana.position = CGPoint(x: player1.position.x - 30, y: player1.position.y + 40)
+            banana.physicsBody?.angularVelocity = -20
+            
+            let raiseArm = SKAction.setTexture(SKTexture(imageNamed: "player1Throw"))
+            let lowerArm = SKAction.setTexture(SKTexture(imageNamed: "player"))
+            let pause = SKAction.wait(forDuration: 0.15)
+            let sequence = SKAction.sequence([raiseArm, pause, lowerArm])
+            player1.run(sequence)
+            
+            let impulse = CGVector(dx: cos(radians) * speed, dy: sin(radians) * speed)
+            banana.physicsBody?.applyImpulse(impulse)
+        } else {
+            banana.position = CGPoint(x: player2.position.x + 30, y: player2.position.y + 40)
+            banana.physicsBody?.angularVelocity = +20
+            
+            let raiseArm = SKAction.setTexture(SKTexture(imageNamed: "player2Throw"))
+            let lowerArm = SKAction.setTexture(SKTexture(imageNamed: "player"))
+            let pause = SKAction.wait(forDuration: 0.15)
+            let sequence = SKAction.sequence([raiseArm, pause, lowerArm])
+            player2.run(sequence)
+            
+            let impulse = CGVector(dx: cos(radians) * -speed, dy: sin(radians) * speed)
+            banana.physicsBody?.applyImpulse(impulse)
+        }
+    }
+    
+    func createPlayers() {
+        player1 = SKSpriteNode(imageNamed: "player")
+        player1.name = "player1"
+        player1.physicsBody = SKPhysicsBody(circleOfRadius: player1.size.width / 2)
+        player1.physicsBody?.categoryBitMask = CollisionTypes.player.rawValue
+        player1.physicsBody?.collisionBitMask = CollisionTypes.banana.rawValue
+        player1.physicsBody?.contactTestBitMask = CollisionTypes.banana.rawValue
+        player1.physicsBody?.isDynamic = false
+        
+        let player1Building = buildings[1]
+        player1.position = CGPoint(x: player1Building.position.x, y: player1Building.position.y + ( (player1Building.size.height + player1.size.height) / 2 ))
+        addChild(player1)
+        
+        player2 = SKSpriteNode(imageNamed: "player")
+        player2.name = "player2"
+        player2.physicsBody = SKPhysicsBody(circleOfRadius: player2.size.width / 2)
+        player2.physicsBody?.categoryBitMask = CollisionTypes.player.rawValue
+        player2.physicsBody?.collisionBitMask = CollisionTypes.banana.rawValue
+        player2.physicsBody?.contactTestBitMask = CollisionTypes.banana.rawValue
+        player2.physicsBody?.isDynamic = false
+        
+        let player2Building = buildings[buildings.count - 2]
+        player2.position = CGPoint(x: player2Building.position.x, y: player2Building.position.y + ( (player2Building.size.height + player2.size.height) / 2 ))
+        addChild(player2)
+    }
+    
+    func deg2rad(degrees: Int) -> Double {
+        return Double(degrees) * .pi / 180
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let firstBody: SKPhysicsBody
+        let secondBody: SKPhysicsBody
+        
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        
+        guard let firstNode = firstBody.node else { return }
+        guard let secondNode = secondBody.node else { return }
+        
+        if firstNode.name == "banana" && secondNode.name == "building" {
+            bananaHit(building: secondNode, atPoint: contact.contactPoint)
+        }
+        
+        if firstNode.name == "banana" && secondNode.name == "player1" {
+            destroy(player: player1)
+        }
+        
+        if firstNode.name == "banana" && secondNode.name == "player2" {
+            destroy(player: player2)
+        }
+    }
+    
+    func destroy(player: SKSpriteNode) {
+        if let explosion = SKEmitterNode(fileNamed: "hitPlayer") {
+            explosion.position = player.position
+            addChild(explosion)
+        }
+        
+        let defaults = UserDefaults.standard
+        
+        if player.name == "player1" {
+            score2 += 1
+            if score2 >= 3 {
+                endGame()
+            } else {
+                defaults.setValue(score2, forKey: "score2")
+            }
+        } else if player.name == "player2" {
+            score1 += 1
+            if score1 >= 3 {
+                endGame()
+            } else {
+                defaults.setValue(score1, forKey: "score1")
+            }
+        }
+        
+        player.removeFromParent()
+        banana.removeFromParent()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            let newGame = GameScene(size: self.size)
+            newGame.viewController = self.viewController
+            self.viewController?.currentGame = newGame
+            
+            self.changePlayer()
+            newGame.currentPlayer = self.currentPlayer
+            
+            let transition = SKTransition.doorway(withDuration: 1.5)
+            self.view?.presentScene(newGame, transition: transition)
+        }
+    }
+    
+    func bananaHit(building: SKNode, atPoint contactPoint: CGPoint) {
+        guard let building = building as? BuildingNode else { return }
+        let buildingLocation = convert(contactPoint, to: building)
+        
+        building.hit(at: buildingLocation)
+        
+        if let explosion = SKEmitterNode(fileNamed: "hitBuilding") {
+            explosion.position = contactPoint
+            addChild(explosion)
+        }
+        
+        banana.name = ""
+        banana.removeFromParent()
+        banana = nil
+        
+        changePlayer()
+    }
+    
+    func changePlayer() {
+        if currentPlayer == 1 {
+            currentPlayer = 2
+        } else {
+            currentPlayer = 1
+        }
+        
+        viewController?.activatePlayer(number: currentPlayer)
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        guard banana != nil else { return }
+        
+        if abs(banana.position.y) > 1000 {
+            banana.removeFromParent()
+            banana = nil
+            changePlayer()
+        }
+    }
+    
+    func endGame() {
+        
+        let defaults = UserDefaults.standard
+        defaults.setValue(0, forKey: "score1")
+        defaults.setValue(0, forKey: "score2")
+        
+        let gameOverLabel = SKLabelNode(fontNamed: "Chalkduster")
+        if score1 >= 3 {
+            gameOverLabel.text = "Game Over | Player1 won"
+        } else if score2 >= 3 {
+            gameOverLabel.text = "Game Over | Player2 won"
+        }
+        gameOverLabel.position = CGPoint(x: 512, y: 368)
+        gameOverLabel.zPosition = 1
+        gameOverLabel.fontSize = 50
+        gameOverLabel.fontColor = UIColor.black
+        gameOverLabel.horizontalAlignmentMode = .center
+        addChild(gameOverLabel)
+        
+        isUserInteractionEnabled = false
+        physicsWorld.speed = 0
+    }
+}
---
+++ 38-Project29/Project29/GameViewController.swift
@@ -0,0 +1,100 @@
+//
+//  GameViewController.swift
+//  Project29
+//
+//  Created by Khumar Girdhar on 24/06/21.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    var currentGame: GameScene?
+    
+    @IBOutlet var angleSlider: UISlider!
+    @IBOutlet var angleLabel: UILabel!
+    @IBOutlet var velocitySlider: UISlider!
+    @IBOutlet var velocityLabel: UILabel!
+    @IBOutlet var launchButton: UIButton!
+    @IBOutlet var playerNumber: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+                
+                currentGame = scene as? GameScene
+                currentGame?.viewController = self
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+            
+            angleChanged(self)
+            velocityChanged(self)
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    @IBAction func angleChanged(_ sender: Any) {
+        angleLabel.text = "Angle: \(Int(angleSlider.value))°"
+    }
+    
+    @IBAction func velocityChanged(_ sender: Any) {
+        velocityLabel.text = "Velocity: \(Int(velocitySlider.value))"
+    }
+    
+    @IBAction func launchTapped(_ sender: Any) {
+        angleSlider.isHidden = true
+        angleLabel.isHidden = true
+        
+        velocityLabel.isHidden = true
+        velocitySlider.isHidden = true
+        
+        launchButton.isHidden = true
+        
+        currentGame?.launch(angle: Int(angleSlider.value), velocity: Int(velocitySlider.value))
+    }
+    
+    func activatePlayer(number: Int) {
+        if number == 1 {
+            playerNumber.text = "<<< PLAYER ONE"
+        } else {
+            playerNumber.text = "PLAYER TWO >>>"
+        }
+        
+        angleSlider.isHidden = false
+        angleLabel.isHidden = false
+        
+        velocityLabel.isHidden = false
+        velocitySlider.isHidden = false
+        
+        launchButton.isHidden = false
+    }
+    
+}
